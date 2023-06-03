6df28d9ca0f16ca868a527f53afc9c7d12750ed7
tragdate <tragdate@gmail.com>
2023-06-04 01:07:25 +0300

    inital push

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# multistream-cli-chat
-A command line interface tool that displays chat from multiple streaming services
+# MultiStream-Cli-Chat
+
+A command-line interface application that connects to YouTube live chat, TikTok live chat, and Twitch chat, displaying the messages in the console.
+
+## Disclaimer 
+
+⚠️ - You should check TOS before using - ⚠️
+
+🤷 - I have no idea what I am doing - 🤷
+
+## Features
+
+- Connects to YouTube, TikTok, and Twitch chats simultaneously
+- Display chat messages in the console with different colors for each platform
+- Resizes chat messages automatically to fit the console width on startup
+
+## Requirements
+
+- Node.js
+- npm
+
+## Installation
+
+1. Clone the repository from GitHub:
+
+```
+git clone https://github.com/tragdate/multistream-cli-chat
+```
+
+2. Change to the project directory:
+
+```
+cd multistream-cli-chat
+```
+
+3. Install the required dependencies:
+
+```
+npm install -g .
+```
+
+## Usage
+
+Run the application with the following command:
+
+```
+multistream-cli-chat --yt=<youtube_channel_id> --tw=<twitch_channel_name> --tt=<tiktok_username>
+```
+
+Replace `<youtube_channel_id>`, `<twitch_channel_name>`, and `<tiktok_username>` with appropriate values.
+
+For example:
+
+```
+multistream-cli-chat --yt="UC3LAmF5-BefGszU5rtYWCTw" --tw=my_twitch_channel --tt=my_tiktok_username
+```
+
+## Author
+
+- [Trag Date](https://tragdate.ninja)
+- [GitHub](https://github.com/tragdate)
+
+## Documentation 
+
+- Made by [DocuTron](https://github.com/tragdate/docuTron)
+
+## License
+
+This project is available under the [MIT License](https://opensource.org/licenses/MIT).
---
+++ index.js
@@ -0,0 +1,103 @@
+#!/usr/bin/env node
+// Name MultiStream-Cli-Chat
+// Description: This is the main file of the project. It will connect to the youtube chat and the tiktok live chat and print the messages to the console.
+// Author: Trag Date
+// github: https://github.com/tragdate
+
+const { WebcastPushConnection } = require('tiktok-live-connector');
+const { LiveChat } = require("youtube-chat")
+const util = require('util');
+const minimist = require('minimist');
+var tw = require('twitch-webchat');
+
+
+const args = minimist(process.argv.slice(2));
+
+const youtubeChat = new LiveChat({channelId: args.yt});
+var channelName = args.tw;
+let tiktokUsername = args.tt;
+
+const wrap = require('word-wrap');
+let yourWidth = process.stdout.columns;
+
+function wrapText(text, width) {
+    const regex = new RegExp(`(?![^\\n]{1,${width}}$)([^\\n]{1,${width}})\\s`, 'g');
+    return text.replace(regex, '$1\n');
+}
+
+process.stdout.on('resize', () => {
+    yourWidth = process.stdout.columns;
+});
+
+var controls = tw.start( channelName, function (err, message) {
+    if (err) throw err
+
+    switch (message.type) {
+        case 'chat': 
+            var user = message.from
+            var text = message.text 
+            var html = message.html 
+            var isModerator = !!message.moderator 
+            var isSubscriber = !!message.subscriber 
+            var isPrime = !!message.prime 
+            var newtext = text.replaceAll(/\bLUL\b/g,"🤣");
+
+            const wrappedText = wrapText('\x1b[35m  \x1b[0m ' + user + ": " + newtext, yourWidth);
+            console.log(wrappedText);
+            break
+        case 'system': 
+            break
+        case 'tick': 
+        case 'debug':
+        default:
+    };
+});
+
+let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
+
+tiktokLiveConnection.connect().then(state => {
+    console.info(`Connected to ${tiktokUsername}'s live chat`);
+}).catch(err => {
+    console.error('Failed to connect', err);
+})
+tiktokLiveConnection.on('chat', data => {
+    const wrappedText = wrapText(` \x1b[40m\x1b[37m \x1b[0m ${data.uniqueId}: ${data.comment}`, yourWidth);
+    console.log(wrappedText);
+
+
+
+})
+tiktokLiveConnection.on('member', data => {
+    console.log(` \x1b[40m\x1b[37m \x1b[0m a intrat ${data.uniqueId}`);
+})
+
+tiktokLiveConnection.on('gift', data => {
+    if (data.giftType === 1 && !data.repeatEnd) {
+        // Streak in progress => show only temporary
+        console.log(` \x1b[40m\x1b[37m \x1b[0m ${data.uniqueId} trimite ${data.giftName} x${data.repeatCount}`);
+    } else {
+        // Streak ended or non-streakable gift => process the gift with final repeat_count
+        console.log(` \x1b[40m\x1b[37m \x1b[0m ${data.uniqueId} trimite ${data.giftName} x${data.repeatCount}`);
+    }
+})
+
+
+
+
+youtubeChat.start().then(ok => {
+    if (!ok) {
+        console.log("Failed to start, check emitted error");
+    }
+
+    youtubeChat.on("chat", (chatItem) => {
+        let messageItems = chatItem.message;
+        let chatText = messageItems.map(item => item.text || (item.emojiText || "")).join("");
+        const wrappedText = wrapText(` \x1b[31m \x1b[0m ${chatItem.author.name}: ${chatText}`, yourWidth);
+        console.log(wrappedText);
+    });
+    youtubeChat.on("error", (err) => {
+    //print youtube error to console if you want
+    })
+}).catch(err => {
+    console.error('Error starting youtubeChat', err);
+});
